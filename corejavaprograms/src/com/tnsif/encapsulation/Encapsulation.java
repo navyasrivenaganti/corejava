01a15084f174c94f93dd28d4287f75cd8df352b6
Dileep Reddy <Dileep Reddy@dileep>
2024-07-03 21:03:19 +0530

    commit message

---
+++ corejavaprograms/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialnumber;
+	String name;
+	int age;
+	void show()
+	{
+		System.out.println("serialnumber"+serialnumber+"name"+name+"age"+age);
+	}
+
+}
---
+++ corejavaprograms/src/com/tnsif/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulationdemo {
+	public static void main(String[] args)
+	{
+		Encapsulation e=new Encapsulation();
+		e.serialnumber=472;
+		e.name="navyasri";
+		e.age=20;
+		e.show();
+	}
+
+}
